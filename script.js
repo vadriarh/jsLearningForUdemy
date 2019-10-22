31a1511e5c4f1a48711be28ca2977745ea2d8f0d
vadriarh <vadriarh@gmail.com>
2019-10-22 23:15:30 +0300

    create first hometask

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-let status="good";
-console.log(status);
+'use strict';
+let money = +prompt("Ваш бюджет на месяц?", ""),
+    time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+function setExpense() {
+    return prompt("Введите обязательную " +
+        "статью расходов в этом месяце", "")
+};
+
+function setAmount() {
+    return +prompt("Во сколько обойдется?", "")
+};
+console.log(typeof(money));
+let appData = {
+
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+
+}
+
+appData.expenses[setExpense()] = setAmount();
+appData.expenses[setExpense()] = setAmount();
+
+console.log(appData);
+alert(appData.budget / 30);
