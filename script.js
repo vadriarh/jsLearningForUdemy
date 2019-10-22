2712229ced039d9a48994c813596a25952cf4aec
vadriarh <vadriarh@gmail.com>
2019-10-22 23:18:49 +0300

    erase some lines

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 let money = +prompt("Ваш бюджет на месяц?", ""),
     time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
@@ -10,20 +10,17 @@ function setExpense() {
 function setAmount() {
     return +prompt("Во сколько обойдется?", "")
 };
-console.log(typeof(money));
-let appData = {
 
+let appData = {
     budget: money,
     timeData: time,
     expenses: {},
     optionalExpenses: {},
     income: [],
     savings: false
-
 }
 
 appData.expenses[setExpense()] = setAmount();
 appData.expenses[setExpense()] = setAmount();
 
-console.log(appData);
 alert(appData.budget / 30);
