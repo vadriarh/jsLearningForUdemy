5ded4941093c1e4deb77ae3bf3dc680535b12ff7
vadriarh <vadriarh@gmail.com>
2019-11-05 09:18:36 +0300

    Refactoring first lesson

---
+++ lesson1/script.js
@@ -0,0 +1,29 @@
+"use strict";
+console.log(status);
+let money = +prompt("Ваш бюджет на месяц?", ""),
+  time = prompt("Введите дату в формате YYYY-MM-DD", "");
+
+function setExpense() {
+  return prompt("Введите обязательную " + "статью расходов в этом месяце", "");
+}
+
+function setAmount() {
+  return +prompt("Во сколько обойдется?", "");
+}
+
+console.log(typeof money);
+
+let appData = {
+  budget: money,
+  timeData: time,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  savings: false
+};
+
+appData.expenses[setExpense()] = setAmount();
+appData.expenses[setExpense()] = setAmount();
+
+console.log(appData);
+alert(appData.budget / 30);
